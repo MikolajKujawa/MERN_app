1d97408e6df6f2f4c5f9b775854ba383a2efe70b
Mikolaj Kujawa <mikolaj.kujawa@erli.pl>
2022-10-22 18:01:07 +0200

    Create structure and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start!</h1>;
+import NewPlace from './places/pages/NewPlace';
+import Users from './user/pages/Users';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Users/>
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace/>
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Users = () => {
+    return <h2>Users Pages</h2>
+}
+
+export default Users;
