a58bfb5f9e3a4361159506c7d1270369d7b4f6a1
Mikołaj Kujawa <mkkujawix@gmail.com>
2022-11-06 20:14:11 +0100

    Create update place page and custom hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
-import NewPlace from './places/pages/NewPlace';
-import UserPlaces from './places/pages/UserPlaces';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-import Users from './user/pages/Users';
+import NewPlace from "./places/pages/NewPlace";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import Users from "./user/pages/Users";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <MainNavigation/>
+      <MainNavigation />
       <main>
         <Switch>
           <Route path="/" exact>
-            <Users/>
+            <Users />
           </Route>
           <Route path="/:userId/places">
-            <UserPlaces/>
-          </Route>            
+            <UserPlaces />
+          </Route>
           <Route path="/places/new" exact>
-            <NewPlace/>
+            <NewPlace />
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace />
           </Route>
           <Redirect to="/" />
         </Switch>
       </main>
-      
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
